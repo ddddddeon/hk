2900fa973ae1c024804d2615f6f586f93fc6df2d
ddddddeon <chris@sweetiq.com>
2016-02-19 16:22:12 +0000

    formatting tweaks

--- src/bit.c
+++ src/bit.c
@@ -11,15 +11,18 @@
  */
 char* hk_int2bin(unsigned int i) {
     size_t bits = sizeof(int) * CHAR_BIT;
-    
     char* str = malloc(bits + 1);
-    if(!str) return NULL;
-    str[bits] = 0;
+    if (!str) {
+	return NULL;
+    }
+
+    str[bits] = HK_ZERO;
     
     unsigned u = *(unsigned *)&i;
-    for(/* void */; bits--; u >>= 1)
+    for (/* void */; bits--; u >>= 1) {
 	str[bits] = u & 1 ? '1' : '0';
-    
+    }
+
     return str;
 }
 
