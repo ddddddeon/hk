382a51dba104412bdd5f97a05d4a68a3243e95c2
ddddddeon <chris@sweetiq.com>
2016-02-19 22:25:32 +0000

    untabify bit.c

--- src/bit.c
+++ src/bit.c
@@ -13,14 +13,14 @@ char* hk_int2bin(unsigned int i) {
     size_t bits = sizeof(int) * CHAR_BIT;
     char* str = malloc(bits + 1);
     if (!str) {
-	return NULL;
+        return NULL;
     }
 
     str[bits] = HK_ZERO;
     
     unsigned u = *(unsigned *)&i;
     for (/* void */; bits--; u >>= 1) {
-	str[bits] = u & 1 ? '1' : '0';
+        str[bits] = u & 1 ? '1' : '0';
     }
 
     return str;
