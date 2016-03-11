19585bcd4b7e8c9b9608f9e54ff1d4451d33592c
ddddddeon <chris@sweetiq.com>
2016-03-11 13:20:58 -0500

    add hk_cmd function

---
+++ src/os.c
@@ -0,0 +1,22 @@
+#include "os.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int hk_cmd(char* cmd) {
+    char buf[BUFSIZE];
+
+    FILE *p = popen(cmd, "r");
+    
+    if (p == NULL) {
+        perror("pipe ");
+        exit(1);
+    }
+
+    while (fgets(buf, sizeof(buf), p) != NULL) {
+        printf("%s\n", buf);
+    }
+
+    return pclose(p);
+   
+}
---
+++ src/os.h
@@ -0,0 +1,8 @@
+#ifndef OS_H
+#define OS_H
+
+#define BUFSIZE 1024
+
+int hk_cmd(char* cmd);
+
+#endif /* !OS_H */
